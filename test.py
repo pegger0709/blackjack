1472d511b5de6c95a0efa69cb75246be10cfff7c
Philip Egger <philip.egger.1987@gmail.com>
2024-06-04 10:41:43 +0200

    Rewrite to facilitate live gameplay

--- test.py
+++ test.py
@@ -1,20 +1,37 @@
+import argparse
+import sys
 from player import *
-player = Player(1000)
-shoe = Shoe(6, 100)
-dealer = Dealer()
-verbose = True
-useBasicStrategy = True
 
-true_count = player.running_count * 52 / shoe.numberOfCards()
-bet = 2 if true_count > 1 else 1
-dealerUpCard = shoe.dealCard('9')
-playerHand = Hand([shoe.dealCard('8'),shoe.dealCard('8')])
-player.playHand(playerHand, dealerUpCard, shoe, bet, useBasicStrategy, verbose)
-dealer.dealHand(Hand([dealerUpCard, shoe.dealCard()]))
-dealer.playHand(shoe, verbose)
-for hand in player.hands+[dealer.hand]:
-    for card in hand.cards: player.addToCount(COUNT_BY_CARD[card.face_value])
+def buildArgsParser():
+    p = argparse.ArgumentParser(
+        description=__doc__, formatter_class=argparse.RawTextHelpFormatter,
+        epilog="")
+    p._optionals.title = "Generic options"
+    p.add_argument('--bankroll', dest='initial_bankroll', type=int, default=1000, help="Initial bankroll of the player.")
+    p.add_argument('--decks', dest='n_decks', type=int, default=-1, help="How many decks to put in the shoe.")
+    p.add_argument('--shuffle', dest='shoe_shuffle', type=int, default=100, help="How many cards to leave undealt in the shoe.")
+    return p
 
-dealer.settlePlayer(player, verbose)
-player
+if __name__ == "__main__":
+    parser = buildArgsParser()
+    args = parser.parse_args()
+    player = Player(args.initial_bankroll)
+    shoe = Shoe(args.n_decks, args.shoe_shuffle)
+    dealer = Dealer()
+    useBasicStrategy = False
+    verbose = True
+    outputFile = sys.stdout
+    while player.bankroll > 0:
+        print(shoe)
+        print(player)
+        true_count = player.running_count * 52 / shoe.numberOfCards()
+        bet = int(input("Please enter the bet: "))
+        dealerUpCard = shoe.dealCard()
+        playerHand = shoe.dealHand()
+        player.playHand(playerHand, dealerUpCard, shoe, bet, useBasicStrategy, verbose, outputFile)
+        dealer.dealHand(Hand([dealerUpCard, shoe.dealCard()]))
+        dealer.playHand(shoe, verbose, outputFile)
+        for hand in player.hands+[dealer.hand]:
+            for card in hand.cards: player.addToCount(COUNT_BY_CARD[card.face_value])
+        dealer.settlePlayer(player, verbose, outputFile)
 
